4f760faff309b9b7ded64a510a0fbbcf16215ff9
Tahi <23127116@student.hcmus.edu.vn>
2025-12-16 18:59:34 +0700

    # refactor: � delete so luong con

--- client/src/components/admin/Categories.tsx
+++ client/src/components/admin/Categories.tsx
@@ -108,11 +108,6 @@ const TreeItem: FC<TreeItemProps> = ({
             {node.name}
           </span>
 
-          {hasChildren && (
-            <span className='px-2 py-0.5 text-xs rounded-full bg-gray-200 text-gray-600 font-medium'>
-              {node.children?.length}
-            </span>
-          )}
         </div>
 
         {isCategory && (

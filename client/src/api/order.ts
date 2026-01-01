61263ccc983c7ac7c6a88f16969ebf2d97b7e8e3
ThanhDang-Vn <ntdang23@clc.fitus.edu.vn>
2026-01-01 23:16:02 +0700

    refactor: :sparkles: completed navigate to payment

--- client/src/api/order.ts
+++ client/src/api/order.ts
@@ -1,21 +1,17 @@
 export const getAllProductByBidder = async (token: string) => {
   try {
-    const res = await fetch(
-      `${import.meta.env.VITE_BACKEND_URL}/orders?view=BIDDER`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        method: "GET",
-      }
-    );
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/orders?view=BIDDER`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'GET',
+    });
 
     const data = await res.json();
 
     if (!res.ok) {
-      const message =
-        data.message || data.errors || "Failed to fetch product sell by user";
+      const message = data.message || data.errors || 'Failed to fetch product sell by user';
       throw new Error(message);
     }
 
@@ -29,21 +25,17 @@ export const getAllProductByBidder = async (token: string) => {
 
 export const getOrderInfo = async (orderId: string, token: string) => {
   try {
-    const res = await fetch(
-      `${import.meta.env.VITE_BACKEND_URL}/orders/${orderId}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        method: "GET",
-      }
-    );
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/orders/${orderId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'GET',
+    });
     const data = await res.json();
 
     if (!res.ok) {
-      const message =
-        data.message || data.errors || "Failed to fetch order info";
+      const message = data.message || data.errors || 'Failed to fetch order info';
       throw new Error(message);
     }
 
@@ -54,7 +46,30 @@ export const getOrderInfo = async (orderId: string, token: string) => {
   }
 };
 
-export const uploadOrderQR = async ( orderId: string, token: string, qrInfo: string, image: File) => {
+export const getOrderByProductId = async (productId: string, token: string) => {
+  try {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/orders/products/${productId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'GET',
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      const message = data.message || data.errors || 'Failed to fetch order by productId';
+      throw new Error(message);
+    }
+
+    return data.data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export const uploadOrderQR = async (orderId: string, token: string, qrInfo: string, image: File) => {
   const formData = new FormData();
   formData.append('qrInfo', qrInfo);
   formData.append('image', image);
@@ -65,7 +80,7 @@ export const uploadOrderQR = async ( orderId: string, token: string, qrInfo: str
       Authorization: `Bearer ${token}`,
     },
     body: formData,
-  })
+  });
 
   const data = await res.json();
 
@@ -74,9 +89,15 @@ export const uploadOrderQR = async ( orderId: string, token: string, qrInfo: str
   }
 
   return data.data;
-}
+};
 
-export const uploadPayment = async ( orderId: string, token: string, buyerAddress: string, buyerPhone: string , image: File) => {
+export const uploadPayment = async (
+  orderId: string,
+  token: string,
+  buyerAddress: string,
+  buyerPhone: string,
+  image: File
+) => {
   const formData = new FormData();
   formData.append('buyerAddress', buyerAddress);
   formData.append('buyerPhone', buyerPhone);
@@ -88,18 +109,18 @@ export const uploadPayment = async ( orderId: string, token: string, buyerAddres
     },
     method: 'PATCH',
     body: formData,
-    })
+  });
 
   const data = await res.json();
-    
+
   if (!res.ok) {
     throw new Error(data.message || data.error || 'Tải thông tin thanh toán thất bại');
   }
 
   return data.data;
-}
+};
 
-export const uploadShippingInfo = async ( orderId: string, token: string, shippingCode: string, image: File) => {
+export const uploadShippingInfo = async (orderId: string, token: string, shippingCode: string, image: File) => {
   const formData = new FormData();
   formData.append('shippingCode', shippingCode);
   formData.append('image', image);
@@ -110,55 +131,58 @@ export const uploadShippingInfo = async ( orderId: string, token: string, shippi
     },
     method: 'PATCH',
     body: formData,
-    })
+  });
 
   const data = await res.json();
-    
+
   if (!res.ok) {
-    throw new Error(data.message || data.error || 'Tải thông tin vận chuyển thất bại'); 
+    throw new Error(data.message || data.error || 'Tải thông tin vận chuyển thất bại');
   }
 
   return data.data;
-}
+};
 
 export const confirmOrder = async (orderId: string, token: string) => {
-   const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/orders/${orderId}/confirm`, {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/orders/${orderId}/confirm`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
     method: 'PATCH',
-   })
+  });
 
   const data = await res.json();
-    
+
   if (!res.ok) {
-    throw new Error(data.message || data.error || 'Xác nhận đơn hàng thất bại'); 
+    throw new Error(data.message || data.error || 'Xác nhận đơn hàng thất bại');
   }
 
   return data.data;
-}
+};
 
+export const ratingOrder = async (
+  token: string,
+  evaluatee: string,
+  productId: string,
+  rating: number,
+  comment: string
+) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/ratings/users/${evaluatee}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      productId: productId,
+      value: rating,
+      comment: comment,
+    }),
+  });
 
-export const ratingOrder = async (token: string, evaluatee:string ,productId: string, rating: number, comment: string) => {
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || data.error || 'Đánh giá thất bại');
+  }
 
-
-    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/ratings/users/${evaluatee}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        productId: productId,
-        value: rating,
-        comment: comment, 
-      })
-    })
-
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || data.error || 'Đánh giá thất bại'); 
-    }
-    
-    return data.data;
-}
+  return data.data;
+};
